2eb8e0a78ce20c20f2c33c6bcb2efc26801c63d1
Laura <lmh95@cornell.edu>
2015-12-15 11:22:55 -0500

    seeing what happens

--- interpolation.c
+++ interpolation.c
@@ -157,8 +157,8 @@ int main(int argc, char** argv) {
                     v_tilde = v[j] + cos(x[i])*dt/2; //THIS IS WHERE E IS
                     printf("x_tilde: %.8f\n", x_tilde);
                     printf("v_tilde: %.8f\n", v_tilde);
-                    x_c = floor((x_tilde-x_0)/dx) + n_l/2;
-                    v_c = floor((v_tilde-v_0)/dv) + n_l/2;
+                    x_c = floor((x_tilde-x_0)/dx) + n_l;
+                    v_c = floor((v_tilde-v_0)/dv) + n_l;
                     printf("x_start_index: %d\n", x_c);
                     printf("v_start_index: %d\n", v_c);
                     for (ii=0; ii<n_l; ii++) {
